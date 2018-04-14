03da50eaac1b15295b5b58402673c8b8341d5e67
YeomanYe <ming13821007683@gmail.com>
2018-04-14 19:35:03 +0800

    update style

--- js/comp/ColList.js
+++ js/comp/ColList.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
 
 export default class ColList extends Component {
+    createDelCol(colItem){
+        let {type,site,title} =colItem;
+        let self = this;
+        return ()=>{
+            getCols(site,type,(cols,allCols)=>{
+                let index = arrEqStr(cols,{title:title});
+                let col;
+                if(index < 0) return;
+                col = cols.splice(index,1);
+                storLocal.set({[STOR_KEY_COLS]:allCols});
+                decUpdateNum(col);
+                //从视图中删除
+                let {datas} = self.state;
+                index = datas.indexOf(colItem);
+                datas.splice(index,1);
+                self.setState({datas});
+                sendToAllTabs([CNT_CMD_UPDATE_CUR_FAV]);
+            });
+        }
+    }
     renderItem(datas){
         let retArr = [];
         datas.map((data)=>{
             retArr.push(
                 <li>
-                    <a href={data.origin} className="left">
+                    <a href={data.origin} target="_blank" className="left">
                         <div style={data.iconStyle}></div>
                     </a>
-                    <div className="middle">{data.title}</div>
-                    <div className="right"><a>删除</a></div>
+                    <div className="middle">
+                    <a href={data.url} target="_blank">
+                        <h3>{data.siteName}<span style={{display:data.isUpdate ? 'inline-block' : 'none'}} className="badge">更新</span></h3>
+                        <p>{data.title}</p>
+                    </a>
+                    </div>
+                    <div className="right">
+                        <p>是否解决:{data.isAccept ? '是' : '否'}</p>
+                        <p>回答数量:{data.answerNum}</p>
+                        <p><span onClick={this.createDelCol(data)} className="del-col">删除</span></p>
+                    </div>
                 </li>
             );
         });
@@ -23,12 +52,12 @@ export default class ColList extends Component {
             allCols = allCols ? allCols : [];
             log('allCols',allCols);
             allCols.map((item)=>{
-                let {icon,origin,siteName,baseUrl} = item;
+                let {icon,origin,siteName,baseUrl,site,type} = item;
                 let iconStyle = {backgroundImage:`url('${icon}')`};
                 item.cols.map((col)=>{
                     let {title,url,isAccept,answerNum} = col;
                     datas.push({
-                        title,isAccept,answerNum,origin,iconStyle,siteName,
+                        type,site,title,isAccept,answerNum,origin,iconStyle,siteName,
                         url:formatHref(url,baseUrl)
                     })
                 });
