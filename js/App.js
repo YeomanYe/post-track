a6d5a4a8cfc2d4f7bdbf1a9d263bc64bea050f57
YeomanYe <ming13821007683@gmail.com>
2019-06-05 16:40:29 +0800

    U webpack.config.js

--- js/App.js
+++ js/App.js
@@ -3,7 +3,7 @@ import {render} from 'react-dom';
 import {Provider} from 'mobx-react';
 import Root from './components/Root';
 import store from './store';
-import scss from '../css/popup.scss';
+import '../css/popup.scss';
 
 const renderDom = Component => {
     render(
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './build'),
         publicPath: '',
-        filename: '[name].js'
+        filename: '[name].[contenthash].js'
     },
     module: {
         rules: [
